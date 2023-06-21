af7cf0f866f1f06ce73739c38de0f8ab1bd75a37
chiemgiabaost <bao2003@my.yorku.ca>
2023-06-21 16:39:03 -0400

    Update website

--- src/components/testinomial/Testinomial.jsx
+++ src/components/testinomial/Testinomial.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './testinomial.css'
 const Testinomial = () => {
   return (
-    <section id='Testinomial'></section>
+    <section id='Testinomial'>
+        <div>
+            
+        </div>
+    </section>
   )
 }
 
